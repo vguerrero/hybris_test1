972e801b8634dc90cf78fcad6cd08a1e3d4c13a9
victor.guerrero <vguerrero.net@gmail.com>
2022-04-21 07:37:00 -0500

    adding extension test

--- bin/custom/concerttours/testsrc/concerttours/jalo/ConcerttoursTest.java
+++ bin/custom/concerttours/testsrc/concerttours/jalo/ConcerttoursTest.java
@@ -6,6 +6,7 @@ package concerttours.jalo;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import de.hybris.bootstrap.annotations.UnitTest;
 import de.hybris.platform.testframework.HybrisJUnit4TransactionalTest;
 
 import org.junit.After;
@@ -13,11 +14,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 /**
  * JUnit Tests for the Concerttours extension.
  */
+@UnitTest
 public class ConcerttoursTest extends HybrisJUnit4TransactionalTest
 {
 	/**
@@ -46,5 +50,19 @@ public class ConcerttoursTest extends HybrisJUnit4TransactionalTest
 	{
 		final boolean testTrue = true;
 		assertThat(testTrue).isTrue();
+	}
+
+	@Test
+	public void testExtensionCreatedOk() {
+		assertTrue("New constants are not there",
+				Files.exists(Path.of("../hybris/bin/custom/concerttours/src/concerttours/constants/ConcerttoursConstants.java")));
+		assertTrue("New services are not there",
+				Files.exists(Path.of("../hybris/bin/custom/concerttours/src/concerttours/service/ConcerttoursService.java")));
+		assertTrue("New default services are not there",
+				Files.exists(Path.of("../hybris/bin/custom/concerttours/src/concerttours/service/impl/DefaultConcerttoursService.java")));
+		assertTrue("New setup is not there",
+				Files.exists(Path.of("../hybris/bin/custom/concerttours/src/concerttours/setup/ConcerttoursSystemSetup.java")));
+		assertTrue("New standalone is not there",
+				Files.exists(Path.of("../hybris/bin/custom/concerttours/src/concerttours/ConcerttoursStandalone.java")));
 	}
 }
